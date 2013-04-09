add75ac67bdeaae7421c3bb2298661df5489d751
Alex Clark <aclark@aclark.net>
2013-04-08 21:23:53 -0400

    Rename

--- aclarknet/__init__.py
+++ aclarknet/__init__.py
@@ -15,21 +15,21 @@ def main(global_config, **settings):
     config.add_route('root', '/')
     config.add_view(
         root, route_name='root',
-        renderer='pythonpackages:templates/root.mak')
+        renderer='aclarknet:templates/root.mak')
 
     config.add_route('about', '/about')
     config.add_view(
-        'pythonpackages.views.about',
+        'aclarknet.views.about',
         route_name='about',
-        renderer='pythonpackages:templates/about.mak')
+        renderer='aclarknet:templates/about.mak')
 
     config.add_route('contact', '/contact')
     config.add_view(
-        'pythonpackages.views.contact',
+        'aclarknet.views.contact',
         route_name='contact',
-        renderer='pythonpackages:templates/contact.mak')
+        renderer='aclarknet:templates/contact.mak')
 
     config.add_static_view(
-        'static', 'pythonpackages:static', cache_max_age=3600)
+        'static', 'aclarknet:static', cache_max_age=3600)
 
     return config.make_wsgi_app()
