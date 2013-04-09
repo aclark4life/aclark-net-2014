4d59b47de4f6a044fb072d2e1b74e7027268fef4
Alex Clark <aclark@aclark.net>
2013-04-08 21:25:42 -0400

    Clean up

--- aclarknet/views.py
+++ aclarknet/views.py
@@ -1,15 +1,3 @@
-from pyramid.httpexceptions import HTTPFound
-from pyramid.security import authenticated_userid
-from pyramid.security import forget
-from pyramid.security import remember
-try:  # Py3
-    from urllib import parse as urlparse
-except:  # Py2
-    import urlparse
-from . import config
-from . import utils
-import json
-import requests
 
 
 def about(request):
