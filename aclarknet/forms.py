318e445400fc1bcc589a2e142efc82b1cf36e603
Alex Clark <aclark@aclark.net>
2013-04-14 21:00:48 -0400

    Wording

--- aclarknet/forms.py
+++ aclarknet/forms.py
@@ -9,11 +9,11 @@ class ContactFormSchema(colander.MappingSchema):
     """
     email = colander.SchemaNode(
         colander.String(),
-        title="Your email address",
+        title="Please tell us your email address",
     )
     msg = colander.SchemaNode(
         colander.String(),
-        title="Your message",
+        title="And how can we help you?",
         validator=colander.Length(1),
         widget=deform.widget.TextAreaWidget(rows=20, cols=120),
     )
