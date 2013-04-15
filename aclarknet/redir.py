2a6a6d67703c73d05344641c8fbb8dca17f7df77
Alex Clark <aclark@aclark.net>
2013-04-14 20:56:37 -0400

    Wording

--- aclarknet/redir.py
+++ aclarknet/redir.py
@@ -1,14 +1,14 @@
 from pyramid.httpexceptions import HTTPMovedPermanently
 
 
-REDIR_TO = 'http://blog.aclark.net'
+REDIR_TARGET = 'http://blog.aclark.net'
 
 
 def blog(request):
     """
     Redirect http://aclark.net/blog to http//blog.aclark.net
     """
-    return HTTPMovedPermanently(location=REDIR_TO)
+    return HTTPMovedPermanently(location=REDIR_TARGET)
 
 
 def blog_entry(request):
@@ -16,11 +16,11 @@ def blog_entry(request):
     Redirect http://aclark.net/blog/{entry} to http://blog.aclark.net/{entry}
     """
     entry = request.matchdict['entry']
-    return HTTPMovedPermanently(location="%s/%s" % (REDIR_TO, entry))
+    return HTTPMovedPermanently(location="%s/%s" % (REDIR_TARGET, entry))
 
 
 def blog_slash(request):
     """
     Redirect http://aclark.net/blog/ to http://blog.aclark.net
     """
-    return HTTPMovedPermanently(location=REDIR_TO)
+    return HTTPMovedPermanently(location=REDIR_TARGET)
